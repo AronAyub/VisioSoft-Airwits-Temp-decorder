a577e0b950eeab2d3017afb8bcb9164371cde50f
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-03-25 10:44:02 +0300

    added python decoder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VisioSoft Temp decorder
  VisioSoft Cold chain device decorder
+ - This is a simple Decorder for a temperature/Voltage monitored Cold chain device, visio soft.
+
+ 
---
+++ decoder_python.py
@@ -0,0 +1,10 @@
+
+def convert_sensor_data(hex_data):
+
+    # Extract temperature and voltage readings from hex data
+    temp_keys = ['TEMP1', 'TEMP2', 'TEMP3', 'TEMP4']
+    temp_values = [(int(hex_data[i:i+4], 16) - 10000) / 100 for i in range(0, 16, 4)]
+
+    volt = int(hex_data[16:20], 16) / 1000
+ 
+    return dict(zip(temp_keys, temp_values), VOLT=volt)
